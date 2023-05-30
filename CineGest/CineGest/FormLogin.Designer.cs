b81c4c4c786504d6ab89a0458f85870c882a34b3
tomascoelho333 <2220883@my.ipleiria.pt>
2023-05-30 17:50:46 +0100

    Update ao form login

--- CineGest/CineGest/FormLogin.Designer.cs
+++ CineGest/CineGest/FormLogin.Designer.cs
@@ -28,12 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.pbLogoLogin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbLogoLogin
+            // 
+            this.pbLogoLogin.Image = ((System.Drawing.Image)(resources.GetObject("pbLogoLogin.Image")));
+            this.pbLogoLogin.Location = new System.Drawing.Point(39, 37);
+            this.pbLogoLogin.Name = "pbLogoLogin";
+            this.pbLogoLogin.Size = new System.Drawing.Size(237, 107);
+            this.pbLogoLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLogoLogin.TabIndex = 0;
+            this.pbLogoLogin.TabStop = false;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(29)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(318, 463);
+            this.ControlBox = false;
+            this.Controls.Add(this.pbLogoLogin);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(29)))), ((int)(((byte)(54)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormLogin";
             this.Text = "FormLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoLogin)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbLogoLogin;
     }
 }
